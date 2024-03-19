19f71bcd12d98c14b9877893508ebfbc59473e92
Connor Keevill <connor.keevill.19@gmail.com>
2024-03-19 23:24:11 +0000

    Fix typing error

--- raytracer/camera/traceables.go
+++ raytracer/camera/traceables.go
@@ -7,7 +7,7 @@ import (
 
 type DemoTraceable struct{}
 
-func (env *DemoTraceable) Trace(ray primitive.Ray) colour.Colour {
+func (env *DemoTraceable) Trace(ray *primitive.Ray) colour.Colour {
 	return colour.RGB8Bit{
 		R: byte((ray.Direction.X + 1) * 128),
 		G: byte((ray.Direction.Y + 1) * 128),
