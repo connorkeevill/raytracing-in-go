0a205c1c130166a1807e463a1398ab026ed8cdba
Connor Keevill <connor.keevill.19@gmail.com>
2024-03-19 22:24:35 +0000

    Create Scene Struct and implement Traceable interface

---
+++ raytracer/scene/scene.go
@@ -0,0 +1,37 @@
+package scene
+
+import (
+	"raytracing-in-go/colour"
+	. "raytracing-in-go/raytracer/primitive"
+)
+
+type Scene struct {
+	Objects []Object
+}
+
+func (scene *Scene) Trace(ray *Ray) colour.Colour {
+	var hits []Hit
+
+	for _, object := range scene.Objects {
+		hits = append(hits, object.Intersect(ray)...)
+	}
+
+	if len(hits) == 0 {
+		col := colour.NewRGB(0, 0, 0)
+		return &col
+	}
+
+	// Get the closest hit
+	closestHit := &hits[0]
+
+	for _, hit := range hits {
+		if hit.T < closestHit.T {
+			closestHit = &hit
+		}
+	}
+
+	var s Traceable = scene
+	position := ray.At(closestHit.T)
+
+	return (*closestHit.Surface).Material().Shade(closestHit, &s, &position, &closestHit.Normal)
+}
