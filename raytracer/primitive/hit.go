cf7b0282b2070058066ac3682b2a5471bf309eac
Connor Keevill <connor.keevill.19@gmail.com>
2024-03-19 22:24:56 +0000

    Update Hit to also have an Object

--- raytracer/primitive/hit.go
+++ raytracer/primitive/hit.go
@@ -2,13 +2,9 @@ package primitive
 
 import "raytracing-in-go/geometry"
 
-type Hittable interface {
-	Material() Material
-}
-
 type Hit struct {
 	T                float64 // The distance along the ray of the hit.
 	IncomingRay      *Ray
-	Surface          *Hittable
+	Surface          *Object
 	Normal, Position geometry.Vector
 }
