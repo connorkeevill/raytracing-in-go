ac4c3ef6c4c61314ff54febf791efd4680513c99
Connor Keevill <connor.keevill.19@gmail.com>
2024-03-19 22:39:47 +0000

    Update main.go to render a scene with a sphere in it

--- main.go
+++ main.go
@@ -5,17 +5,22 @@ import (
 	"raytracing-in-go/geometry"
 	"raytracing-in-go/image"
 	"raytracing-in-go/raytracer/camera"
+	"raytracing-in-go/raytracer/object"
+	"raytracing-in-go/raytracer/primitive"
+	"raytracing-in-go/raytracer/scene"
 )
 
 func main() {
-	cam := camera.New(geometry.Vector{}, geometry.Vector{Z: 1}, camera.Resolution{Width: 5000, Height: 5000}, 170)
+	environment := scene.Scene{}
+	environment.Objects = append(environment.Objects, &object.Sphere{
+		M:      &primitive.RayDirectionMaterial{},
+		Center: geometry.Vector{Z: 3},
+		Radius: 2})
 
-	tracingEnv := camera.DemoTraceable{}
-	picture := cam.Render(&tracingEnv)
+	cam := camera.New(geometry.Vector{}, geometry.Vector{Z: 1}, camera.Resolution{Width: 1000, Height: 1000}, 90)
+	picture := cam.Render(&environment)
 
 	fmt.Println("Rendered, writing to file")
-
 	image.WriteToFile(picture, "image.ppm")
-
 	fmt.Println("Written")
 }
