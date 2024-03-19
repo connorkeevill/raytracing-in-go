1fbe06651cfc444d2e26e83a013d0776aa4f3a77
Connor Keevill <connor.keevill.19@gmail.com>
2024-03-19 08:28:15 +0000

    Implement a material and add some fields to the Hit type

--- raytracer/primitive/hit.go
+++ raytracer/primitive/hit.go
@@ -3,11 +3,12 @@ package primitive
 import "raytracing-in-go/geometry"
 
 type Hittable interface {
-	GetMaterial() Material
+	Material() Material
 }
 
 type Hit struct {
-	IncomingRay *Ray
-	What        *Hittable
-	Normal      geometry.Vector
+	T                float64 // The distance along the ray of the hit.
+	IncomingRay      *Ray
+	Surface          *Hittable
+	Normal, Position geometry.Vector
 }
--- raytracer/primitive/material.go
+++ raytracer/primitive/material.go
@@ -1,4 +1,32 @@
 package primitive
 
+import (
+	"raytracing-in-go/colour"
+	"raytracing-in-go/geometry"
+)
+
+type Traceable interface {
+	Trace(ray *Ray) colour.Colour
+}
+
 type Material interface {
+	Shade(
+		hit *Hit,
+		scene *Traceable,
+		position, normal *geometry.Vector) colour.Colour
+}
+
+type RayDirectionMaterial struct{}
+
+func (mat *RayDirectionMaterial) Shade(hit *Hit, scene *Traceable, position, normal *geometry.Vector) colour.Colour {
+	// Hit normal should already be normalised, but ensure:
+	hit.Normal.Normalise()
+
+	matColour := colour.New8Bit(
+		byte((hit.Normal.X+1)*128),
+		byte((hit.Normal.Y+1)*128),
+		byte((hit.Normal.Z+1)*128),
+	)
+
+	return &matColour
 }
